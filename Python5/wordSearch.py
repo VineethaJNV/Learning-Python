30d05bbbd95ec3df3d2d7b5a7cef64564e519e1f
VineethaJNV <vineethalekkalaoo3@gmail.com>
2025-11-29 19:26:52 +0530

    Searching for a given word in a .txt file

--- Python5/wordSearch.py
+++ Python5/wordSearch.py
@@ -1,11 +1,9 @@
 with open("wordSearch.txt", "r") as f:
-
-# data = f.read()
-
-    word_to_search = "Python"
+    word_to_search = "python"
     count = 0
-    while(f != None):
-        line = f.readline()
+    data = f.read()
+    lines = data.split(" ")
+    for line in lines:
         count+=1
-        if(word_to_search in line):
-            print(f"{word_to_search} is found in line number {count}")
+        if word_to_search in line:
+            print(f"{word_to_search} exists in the file on line number{count}")
