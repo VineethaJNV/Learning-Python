b0a0f2617be4550db5dffb766766bc451da33871
VineethaJNV <vineethalekkalaoo3@gmail.com>
2025-12-02 20:34:24 +0530

    classes&objects

---
+++ Python4/stuObj.py
@@ -0,0 +1,13 @@
+class Student:
+    subject = "Python"
+    college = "RGUKT"
+    year = "passed out"
+
+student1 = Student()
+student2 = Student()
+
+print(student1.subject, student1.college, student1.year)
+print(student1)
+
+print(student2.subject, student2.college, student2.year)
+print(student2)
