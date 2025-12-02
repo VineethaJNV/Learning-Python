3aa5e36e1a8f3fe517765b26f0fa72a9668ed7f9
VineethaJNV <vineethalekkalaoo3@gmail.com>
2025-12-02 20:59:09 +0530

    Instance/Class attributes in python

---
+++ Python4/attributes.py
@@ -0,0 +1,22 @@
+#class Attributes
+
+#Instance Attributes
+
+class Student:
+    college = "RGUKT"
+    PI = 3.1
+    def __init__(self, name, subject, cgpa):
+        self.name = name
+        self.subject = subject
+        self.cgpa = cgpa
+        self.PI = 3.14
+
+stu1 = Student("Vineetha", "Python", 9.5)
+print(f"The student name is {stu1.name}, Subject is {stu1.subject}, cgpa is {stu1.cgpa}")
+print(stu1.college)#class attribute
+print(Student.college)#class attribute
+
+
+print(stu1.PI)
+#Instance attributes have higher priority, we can use instance to access class attributes as well
+
